2c4681648bcbc8ab0c2940d24087a82bf8157e28
NaaQ <stevencopy01@gmail.com>
2024-04-11 02:17:25 +0200

    update: add landing to navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -32,6 +32,9 @@ function Navbar() {
           <li>
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></a>
           </li>
+          <li>
+            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#landing"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">LANDING</div></a>
+          </li>
         </ul>
       </div>
     </nav>
