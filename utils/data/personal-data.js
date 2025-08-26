6977e1a11df9a731c02810f60d81370e4d4682f7
Steven <45352533+theejkb@users.noreply.github.com>
2025-08-26 22:42:19 +0200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,12 +2,12 @@ export const personalData = {
   name: "COPY STEVEN",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is COPY STEVEN. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Typescript and I love to do most of the things using Typescript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'stevencopy01@@gmail.com',
+  description: "My name is COPY STEVEN, a professional and enthusiastic web developer. I am a fast learner with a strong self-learning mindset, always eager to explore new technologies and tackle challenges. Passionate about building modern and scalable web applications, I focus on creating efficient and user-friendly solutions. My main expertise lies in TypeScript, which I use extensively across frontend and backend development. I am open to job opportunities that match my skills and allow me to keep growing as a developer.",
+  email: 'stevencopy01@gmail.com',
   phone: '+33635116847',
   address: 'Lyon, France',
   github: 'https://github.com/theejkb',
   linkedIn: 'https://www.linkedin.com/in/stevennaaq/',
   devUsername: "naaq",
-  resume: "https://drive.google.com/file/d/1aMNLRi_EyUMnqTDtF4l0CFwZiBZoW3yy/view?usp=drive_link"
+  resume: "https://cvdesignr.com/p/66154a8a1989d"
 }
