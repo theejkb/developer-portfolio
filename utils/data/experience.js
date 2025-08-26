70e57199559c37caceef667ec04f240e17034db4
Steven <45352533+theejkb@users.noreply.github.com>
2025-08-26 22:44:21 +0200

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,19 +2,19 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer',
+    company: "SopraSteria",
+    duration: "Jan 2025 - Now"
+  },
+  {
+    id: 2,
+    title: 'Software Engineer',
     company: "Sogelink",
     duration: "Oct 2023 - April 2024"
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Engineer",
     company: "Axopen",
     duration: "Sept 2022 - Sept 2023"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "Jan 2020 - Present"
   }
 ]
